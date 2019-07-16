d61f89fcac10111ce5d5c55bdc17bf8a5605a461
s-leirbag <gabriel@shiu.us>
2019-07-16 10:45:15 -0700

    added double jump, cleanup

--- Player.gd
+++ Player.gd
@@ -5,11 +5,12 @@ const GRAVITY = 20
 const ACCEL = 50
 const MAX_SPEED = 200
 const ROLL_SPEED = 300
-const JUMP_HEIGHT = -400
+const JUMP_HEIGHT = -300
 
 var motion = Vector2()
 var state = "move"
 var dir = 1
+var has_jump = true
 
 func _physics_process(delta):
 	motion.y += GRAVITY
@@ -20,14 +21,20 @@ func _physics_process(delta):
 			state = "attack1"
 			$AnimatedSprite.play("attack1")
 			$AnimatedSprite.offset.x = 5 * dir
+			get_parent().get_node("Hitbox")
 		elif Input.is_action_pressed("shift"):
 			motion.x = ROLL_SPEED * dir
 			state = "roll"
 			$AnimatedSprite.play("roll")
 			$AnimatedSprite.offset.x = -7 * dir
 			friction = false
-		elif Input.is_action_pressed("w") and is_on_floor():
-			motion.y = JUMP_HEIGHT
+		elif Input.is_action_just_pressed("w"):
+			if is_on_floor():
+				motion.y = JUMP_HEIGHT
+				has_jump = true
+			elif has_jump:
+				motion.y = JUMP_HEIGHT
+				has_jump = false
 		elif Input.is_action_pressed("d"):
 			motion.x = min(motion.x + ACCEL, MAX_SPEED)
 			dir = 1
@@ -48,8 +55,6 @@ func _physics_process(delta):
 	elif state == "roll":
 		friction = false
 	elif state == "attack1":
-#		if $AnimatedSprite.frame == 0:
-			
 		if $AnimatedSprite.frame >= 2 and $AnimatedSprite.frame <= 4 and Input.is_action_just_pressed("lclick"):
 			state = "attack2"
 			$AnimatedSprite.play("attack2")
